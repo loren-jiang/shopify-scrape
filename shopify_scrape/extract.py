71d9336cc71b4b9303d469a267a75e4a8740a8a7
loren-jiang <loren.jiang@gmail.com>
2020-07-18 22:57:50 -0700

    implemented get_collections and added some tests

--- shopify_scrape/extract.py
+++ shopify_scrape/extract.py
@@ -1,12 +1,67 @@
 import os
 import requests
 import json
+import datetime
 from urllib.parse import urlparse, ParseResult
 from shopify_scrape.utils import format_url
 import gzip  # gzip compresse file sizes by about factor of 10
 
 
-def get_products(url, page_range=None, use_collections=False):
+def extract(endpoint, agg_key, page_range=None):
+    r_list = []
+    if page_range is not None:
+        if type(page_range) != tuple:
+            raise Exception("'page_range' arg must be a 'tuple' object")
+        r_list = list(range(page_range[0], page_range[1]+1))
+        if not r_list or (r_list[0] < 1 or r_list[-1] < 1):
+            raise Exception(
+                "'page_range' must be valid positive, non-zero range; e.g. (1,20)")
+
+    page = 1
+    ret = {
+        agg_key: [],
+        'endpoint_attempted': endpoint,
+        'attempted_at': datetime.datetime.now(),
+        'success': False,
+        'error': ''
+    }
+
+    try:
+
+        while True:
+            page_endpoint = endpoint + f'?page={str(page)}'
+            response = requests.get(page_endpoint)
+            response.raise_for_status()
+            if not response.headers['Content-Type'] == 'application/json; charset=utf-8':
+                raise Exception('Incorrect response content type')
+            data = response.json()
+            page_has_products = agg_key in data and len(
+                data[agg_key]) > 0
+
+            page_in_range = page in r_list or page_range is None
+
+            # break loop if empty or just want first page
+            if not page_has_products or not page_in_range:
+                break
+            ret[agg_key] += data[agg_key]
+            page += 1
+
+        ret['success'] = True
+
+    # Exception handling
+
+    # If 4XX or 5XX status code
+    except requests.exceptions.HTTPError as err:
+        ret['error'] = err
+    except json.decoder.JSONDecodeError as err:
+        ret['error'] = err
+    except Exception as err:
+        ret['error'] = err
+
+    return ret
+
+
+def get_products(url, page_range=None):
     """Takes URL of Shopify store and attempts to get '/products.json' endpoint
 
     Args:
@@ -15,51 +70,22 @@ def get_products(url, page_range=None, use_collections=False):
     Returns:
         Dict: JSON data as dict
     """
-    r_list = []
-    if page_range is not None:
-        if type(page_range) != tuple:
-            raise Exception("'page_range' arg must be a 'tuple' object")
-        r_list = list(range(page_range[0], page_range[0]+1))
-        if not r_list or r_list[0] > 0 and r_list[-1] > 0:
-            raise Exception(
-                "'page_range' must be valid positive range; e.g. (1,20)")
 
     endpoint = f'{url}/products.json'
-    page = 1
-    all_products = {'products': []}
+    return extract(endpoint, 'products', page_range)
 
-    try:
 
-        while True:
-            page_endpoint = endpoint + f'?page={str(page)}'
-            response = requests.get(page_endpoint)
-            response.raise_for_status()
-            data = response.json()
-            page_has_products = 'products' in data and len(
-                data['products']) > 0
+def get_collections(url, page_range=None):
+    """Takes URL of Shopify store and attempts to get '/products.json' endpoint
 
-            page_in_range = page in r_list or page_range is None
+    Args:
+        url ([type]): [description]
 
-            # break loop if 'products' empty or just want first page
-            if not page_has_products or not page_in_range:
-                break
-            all_products['products'] += data['products']
-            page += 1
-
-        if use_collections:
-            # TODO: implement collections when use_collections arg is True
-            pass
-
-    # Exception handling
-    # TODO: handle exceptions better
-    except requests.exceptions.HTTPError as err:
-        print(err)
-    except json.decoder.JSONDecodeError as err:
-        print(err)
-    except Exception as err:  # catches all Exceptions
-        print(err)
-
-    return all_products
+    Returns:
+        Dict: JSON data as dict
+    """
+    endpoint = f'{url}/collections.json'
+    return extract(endpoint, 'collections', page_range)
 
 
 if __name__ == "__main__":
--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,6 +1,32 @@
-from shopify_scrape.extract import get_products
+import pytest
+from shopify_scrape.extract import get_products, get_collections
 
 
 def test_get_products():
     data = get_products('https://bombas.com')
     assert 'products' in data and len(data['products']) > 0
+
+
+def test_get_products_with_page_range():
+    data = get_products('https://bombas.com', page_range=(1, 3))
+    assert 'products' in data and len(data['products']) == 30*3
+
+@pytest.mark.parametrize("test_url, page_range, expectation",
+                         [
+                             ('https://bombas.com', (-1, 3),
+                              pytest.raises(Exception)),
+                              ('https://bombas.com', (3, 1),
+                              pytest.raises(Exception))
+                         ]
+                         )
+def test_get_products_page_range_exceptions(test_url, page_range, expectation):
+    with expectation:
+        assert get_products(test_url, page_range) is not None
+
+def test_get_products_bad_url():
+    data = get_products('https://google.com')
+    assert data['success'] == False, "Unsuccessful 'products.json' retrieval"
+
+def test_get_collections():
+    data = get_collections('https://bombas.com')
+    assert 'collections' in data and len(data['collections']) > 0
